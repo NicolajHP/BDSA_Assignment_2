3c9c30e615ec5fb9f422698c840dea023c384672
Nicol <nihp>
2022-09-20 00:18:46 +0200

    All setup Wizards first query and test finished, added wizards

--- Assignment2.Tests/QueriesTests.cs
+++ Assignment2.Tests/QueriesTests.cs
@@ -5,6 +5,12 @@ public class QueriesTests
     [Fact]
     public void Test1()
     {
-
+        //Arrange
+        var wizards = WizardCollection.Create();
+        //Act
+        var wizbyrowl = Queries.WizardsByRowling(wizards);
+        var wizardsbyJK = new List<String> {"harry", "vold"};
+        //Assert
+        wizbyrowl.Should().BeEquivalentTo("Lord Voldemort", "Harry Potter");
     }
 }
--- Assignment2/Queries.cs
+++ Assignment2/Queries.cs
@@ -2,4 +2,10 @@ namespace Assignment2;
 
 public class Queries
 {
+    public static List<String> WizardsByRowling(WizardCollection wizCol){
+        var wizByRowl = from w in wizCol
+                       where w.Creator.Contains("J.K. Rowling")
+                       select w.Name;
+        return wizByRowl.ToList();
+    }
 }
