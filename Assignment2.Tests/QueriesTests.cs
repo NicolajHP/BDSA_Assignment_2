ae7d95ad1905d427f2500adfdad070d5b2af21e4
Nicol <nihp>
2022-09-20 13:52:50 +0200

    linq queries linq queries almost done, missing last test, and then the extention implementations

--- Assignment2.Tests/QueriesTests.cs
+++ Assignment2.Tests/QueriesTests.cs
@@ -3,14 +3,46 @@ namespace Assignment2.Tests;
 public class QueriesTests
 {
     [Fact]
-    public void Test1()
+    public void Test_Wizards_By_JKRowling_returns_LordVoldemort_and_HarryPotter()
     {
         //Arrange
         var wizards = WizardCollection.Create();
         //Act
         var wizbyrowl = Queries.WizardsByRowling(wizards);
-        var wizardsbyJK = new List<String> {"harry", "vold"};
         //Assert
         wizbyrowl.Should().BeEquivalentTo("Lord Voldemort", "Harry Potter");
+    }
+
+    [Fact]
+    public void Test_First_Sith_Returns_DarthVader()
+    {
+        //Arrange
+        var wizards = WizardCollection.Create();
+        //Act
+        var wizbyrowl = Queries.firstSithAppearance(wizards);
+        //Assert
+        wizbyrowl.Should().BeEquivalentTo("Darth Vader");
+    }
+
+    [Fact]
+    public void Test_Return_all_Wizards_From_HP()
+    {
+        //Arrange
+        var wizards = WizardCollection.Create();
+        //Act
+        var wizbyrowl = Queries.AllWizardsInHP(wizards);
+        //Assert
+        wizbyrowl.Should().BeEquivalentTo(new[] {("Lord Voldemort", 1997), ("Harry Potter", 1997)});
+    }
+
+    [Fact]
+    public void Test_list_By_Creator()
+    {
+        //Arrange
+        var wizards = WizardCollection.Create();
+        //Act
+        var wizbyrowl = Queries.ListOfWizByCreator(wizards);
+        //Assert
+        wizbyrowl.Take(2).Should().BeEquivalentTo(new[] {("Lord Voldemort", 1997), ("Harry Potter", 1997)});
     }
 }
--- Assignment2/Queries.cs
+++ Assignment2/Queries.cs
@@ -8,4 +8,31 @@ public class Queries
                        select w.Name;
         return wizByRowl.ToList();
     }
+
+    public static List<String> firstSithAppearance(WizardCollection wizCol){
+        var FirstSith = from w in wizCol
+                        where w.Name.Contains("Darth")
+                        let y = w.Year
+                        orderby y ascending
+                        select w.Name;
+        return FirstSith.Take(1).ToList();
+    }
+
+    public static IEnumerable<(String Name, int? Year)> AllWizardsInHP(WizardCollection wizCol){
+        var allWiz = from w in wizCol
+                     where w.Medium.Contains("Harry Potter")
+                     select (w.Name, w.Year);
+        return allWiz;
+    }
+
+    public static IEnumerable<System.Linq.IGrouping<String, Assignment2.Wizard>> ListOfWizByCreator(WizardCollection wizCol){
+        var listWiz = from w in wizCol
+                      let c = w.Creator
+                      let n = w.Name
+                      orderby c descending, n descending
+                      group w by c into newGrp
+                      select newGrp;
+        return listWiz;
+
+    }
 }
