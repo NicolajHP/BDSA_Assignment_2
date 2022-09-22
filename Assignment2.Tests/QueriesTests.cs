e9a4c073cf257ffa6bbbb131d63aa3f4267ce13b
Nicol <nihp>
2022-09-22 13:49:34 +0200

    All extension queries are finished

--- Assignment2.Tests/QueriesTests.cs
+++ Assignment2.Tests/QueriesTests.cs
@@ -14,6 +14,17 @@ public class QueriesTests
     }
 
     [Fact]
+    public void Test_Wizards_By_JKRowling_returns_LordVoldemort_and_HarryPotter_With_Extentions()
+    {
+        //Arrange
+        var wizards = WizardCollection.Create();
+        //Act
+        var wizbyrowl = Queries.WizardsByRowlingExtenstion(wizards);
+        //Assert
+        wizbyrowl.Should().BeEquivalentTo("Lord Voldemort", "Harry Potter");
+    }
+
+    [Fact]
     public void Test_First_Sith_Returns_DarthVader()
     {
         //Arrange
@@ -25,12 +36,34 @@ public class QueriesTests
     }
 
     [Fact]
+    public void Test_First_Sith_Returns_DarthVader_Extension()
+    {
+        //Arrange
+        var wizards = WizardCollection.Create();
+        //Act
+        var wizbyrowl = Queries.firstSithAppearanceExtension(wizards);
+        //Assert
+        wizbyrowl.Should().BeEquivalentTo("Darth Vader");
+    }
+
+    [Fact]
     public void Test_Return_all_Wizards_From_HP()
     {
         //Arrange
         var wizards = WizardCollection.Create();
         //Act
         var wizbyrowl = Queries.AllWizardsInHP(wizards);
+        //Assert
+        wizbyrowl.Should().BeEquivalentTo(new[] {("Lord Voldemort", 1997), ("Harry Potter", 1997)});
+    }
+
+    [Fact]
+        public void Test_Return_all_Wizards_From_HP_Extension()
+    {
+        //Arrange
+        var wizards = WizardCollection.Create();
+        //Act
+        var wizbyrowl = Queries.AllWizardsInHPExtension(wizards);
         //Assert
         wizbyrowl.Should().BeEquivalentTo(new[] {("Lord Voldemort", 1997), ("Harry Potter", 1997)});
     }
@@ -51,6 +84,28 @@ public class QueriesTests
         
         //Act
         var wizbyrowl = Queries.ListOfWizByCreator(wizards);
+        
+        //Assert
+        wizbyrowl.Should().BeEquivalentTo(ListList);
+        
+    }
+
+    [Fact]
+    public void Test_list_By_CreatorExtension()
+    {
+        //Arrange
+        var wizards = WizardCollection.Create();
+        var ListList = new List<List<Wizard>>();
+        ListList.Add(new List<Wizard>{new Wizard("Mickey Mouse","Fantasia",1940,"Walt Disney")});
+        ListList.Add(new List<Wizard>{new Wizard("Glinda The Good Witch","The Wizard of Oz",1939,"L. Frank Baum")});
+        ListList.Add(new List<Wizard>{new Wizard("Zatanna Zatara","hawkman #4",1964,"Julius Schwartz")});
+        ListList.Add(new List<Wizard>{new Wizard("Sauron","The Fellowship of the Ring",1954,"J.R.R. Tolkien"), new Wizard("Gandalf","The Fellowship of the Ring",1954,"J.R.R. Tolkien")});
+        ListList.Add(new List<Wizard>{new Wizard("Lord Voldemort","Harry Potter and The Philosopher's Stone",1997,"J.K. Rowling"), new Wizard("Harry Potter","Harry Potter and The Philosopher's Stone",1997,"J.K. Rowling")});
+        ListList.Add(new List<Wizard>{new Wizard("Darth Vader","Star Wars",1977,"George Lucas"), new Wizard("Darth Sidious","Star Wars",1980,"George Lucas")});
+        ListList.Add(new List<Wizard>{new Wizard("Merlin","Prophetiae Merlini",1200,"Geoffrey of Monmouth")});
+        
+        //Act
+        var wizbyrowl = Queries.ListOfWizByCreatorExtension(wizards);
         
         //Assert
         wizbyrowl.Should().BeEquivalentTo(ListList);
--- Assignment2/Queries.cs
+++ Assignment2/Queries.cs
@@ -2,12 +2,22 @@ namespace Assignment2;
 
 public class Queries
 {
+
+    //Queries that returns wizards made by J.K.Rowling
     public static List<String> WizardsByRowling(WizardCollection wizCol){
         var wizByRowl = from w in wizCol
                        where w.Creator.Contains("J.K. Rowling")
                        select w.Name;
         return wizByRowl.ToList();
     }
+
+    public static List<String> WizardsByRowlingExtenstion(WizardCollection wizCol){
+        var wizByRowl = wizCol.Where(w => w.Creator == "J.K. Rowling")
+                        .Select(w => w.Name).ToList();
+        return wizByRowl;
+    }
+
+    //Queries that returns the sith with the earliest appearance.
 
     public static List<String> firstSithAppearance(WizardCollection wizCol){
         var FirstSith = from w in wizCol
@@ -18,6 +28,14 @@ public class Queries
         return FirstSith.Take(1).ToList();
     }
 
+    public static List<String> firstSithAppearanceExtension(WizardCollection wizCol){
+        var FirstSith = wizCol.Where(w => w.Name.Contains("Darth"))
+                        .OrderBy(w => w.Year)
+                        .Select(w => w.Name);
+        return FirstSith.Take(1).ToList();
+    }
+
+    //Queries that return all wizards in the "Harry Potter" medium
     public static IEnumerable<(String Name, int? Year)> AllWizardsInHP(WizardCollection wizCol){
         var allWiz = from w in wizCol
                      where w.Medium.Contains("Harry Potter")
@@ -25,6 +43,14 @@ public class Queries
         return allWiz;
     }
 
+        public static IEnumerable<(String Name, int? Year)> AllWizardsInHPExtension(WizardCollection wizCol){
+        var allWiz = wizCol.Where(w => w.Medium.Contains("Harry Potter"))
+                     .Select(w => (w.Name, w.Year));
+        return allWiz;
+    }
+
+
+    //Querues that return the full list of wizards, ordered in reverse by creator, then by name.
     public static List<List<Wizard>> ListOfWizByCreator(WizardCollection wizCol){
         var listWiz = from w in wizCol
                       let c = w.Creator
@@ -33,6 +59,14 @@ public class Queries
                       group w by c into newGrp
                       select newGrp.ToList();
         return listWiz.ToList();
+
+    }
+
+    public static IEnumerable<System.Linq.IGrouping<String, Wizard>> ListOfWizByCreatorExtension(WizardCollection wizCol){
+        var listWiz = wizCol.OrderByDescending(w => w.Creator)
+                      .ThenByDescending(w => w.Name)
+                      .GroupBy(w => w.Creator);
+        return listWiz;
 
     }
 }
